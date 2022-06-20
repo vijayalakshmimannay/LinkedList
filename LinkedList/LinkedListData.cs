12cc058b649d397e7ef9614a56a7f180d713f10a
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 20:36:06 +0530

    [vijayalakshmi] append

--- LinkedList/LinkedListData.cs
+++ LinkedList/LinkedListData.cs
@@ -133,6 +133,24 @@ namespace LinkedList
             }
             Console.WriteLine("{0} inserted into linked list", node.data);
         }
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Append into linked list", node.data);
+        }
 
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,12 +9,17 @@ namespace LinkedList
         {
             Console.WriteLine("Hello, World!");
             LinkedListData list = new LinkedListData();
-           // list.Add(56);
-           // list.Add(30);
-            list.Add(70);
-            list.AddMethod(30);
-            list.AddMethod(56);
+            // list.Add(56);
+            // list.Add(30);
+            // list.Add(70);
+            // list.AddMethod(30);
+            //  list.AddMethod(56);
+            //  list.DisPlay();
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
             list.DisPlay();
+
 
         }
     }
